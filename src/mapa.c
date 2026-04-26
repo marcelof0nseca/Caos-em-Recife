59ad405979cd7cc66aafe40496c7d39e2cbf94c6
marcelof0nseca <maf@cesar.school>
2026-04-26 20:45:29 -0300

    style: adiciona detalhes simples na grama

--- src/mapa.c
+++ src/mapa.c
@@ -14,6 +14,11 @@ void DesenharMapa(void)
 
         if (cor.r == DARKGRAY.r) {
             DrawLine(0, linha * TAM_BLOCO + 20, LARGURA_TELA, linha * TAM_BLOCO + 20, YELLOW);
+        } else {
+            for (int coluna = 0; coluna < TOTAL_COLUNAS; coluna++) {
+                DrawCircle(coluna * TAM_BLOCO + 12, linha * TAM_BLOCO + 28, 2, DARKGREEN);
+                DrawCircle(coluna * TAM_BLOCO + 30, linha * TAM_BLOCO + 10, 2, DARKGREEN);
+            }
         }
     }
 }
