eccfa1026956bfc32bb7fcc5a0432ccf87b3c5c9
marcelof0nseca <maf@cesar.school>
2026-04-29 19:05:48 -0300

    feat: adiciona sistema de fases iniciais

--- include/jogo.h
+++ include/jogo.h
@@ -4,14 +4,22 @@
 #include <stdbool.h>
 #include "jogador.h"
 #include "obstaculo.h"
+#include "moeda.h"
+
+#define TOTAL_FASES_ATUAIS 2
+#define MAX_MOEDAS 6
 
 typedef struct {
     Jogador jogador;
     Obstaculo *obstaculos;
+    Moeda moedas[MAX_MOEDAS];
+    int totalMoedas;
+    int faseAtual;
     bool gameOver;
     bool jogoIniciado;
     bool venceu;
     bool pausado;
+    bool tocouTopoNestaFase;
     int recorde;
 } Jogo;
 
--- src/jogo.c
+++ src/jogo.c
@@ -3,16 +3,104 @@
 #include "config.h"
 #include "score.h"
 
-void IniciarJogo(Jogo *jogo)
+static void AtualizarRecorde(Jogo *jogo)
+{
+    if (jogo->jogador.score > jogo->recorde) {
+        jogo->recorde = jogo->jogador.score;
+        SalvarRecorde(jogo->recorde);
+    }
+}
+
+static void AdicionarMoedaFase(Jogo *jogo, int coluna, int linha)
+{
+    if (jogo->totalMoedas >= MAX_MOEDAS) {
+        return;
+    }
+
+    IniciarMoeda(&jogo->moedas[jogo->totalMoedas], coluna, linha);
+    jogo->totalMoedas++;
+}
+
+static void ConfigurarMoedas(Jogo *jogo)
+{
+    jogo->totalMoedas = 0;
+
+    if (jogo->faseAtual == 1) {
+        AdicionarMoedaFase(jogo, 4, 11);
+        AdicionarMoedaFase(jogo, 14, 7);
+        AdicionarMoedaFase(jogo, 8, 3);
+        return;
+    }
+
+    AdicionarMoedaFase(jogo, 2, 12);
+    AdicionarMoedaFase(jogo, 16, 8);
+    AdicionarMoedaFase(jogo, 5, 6);
+    AdicionarMoedaFase(jogo, 12, 2);
+}
+
+static void ConfigurarObstaculos(Jogo *jogo)
 {
     LiberarObstaculos(&jogo->obstaculos);
-    IniciarJogador(&jogo->jogador);
     jogo->obstaculos = NULL;
-    AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_CARRO, X_INICIAL_CARRO, Y_INICIAL_CARRO, VELOCIDADE_CARRO, 1));
+
+    AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_CARRO, X_INICIAL_CARRO, 280, VELOCIDADE_CARRO, 1));
     AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_CARRO, LARGURA_TELA + 80, 360, 130, -1));
     AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_ONIBUS, -220, 200, VELOCIDADE_CARRO, 1));
-    AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_BURACO, 240, 240, 0, 0));
-    AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_BURACO, 520, 320, 0, 0));
+
+    if (jogo->faseAtual >= 2) {
+        AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_MOTO, LARGURA_TELA + 120, 240, 0, -1));
+        AdicionarObstaculo(&jogo->obstaculos, CriarObstaculo(TIPO_MOTO, -140, 320, 0, 1));
+    }
+}
+
+static void ConfigurarFase(Jogo *jogo)
+{
+    ConfigurarObstaculos(jogo);
+    ConfigurarMoedas(jogo);
+}
+
+static void AtualizarMoedas(Jogo *jogo)
+{
+    for (int i = 0; i < jogo->totalMoedas; i++) {
+        if (ColetarMoeda(&jogo->moedas[i], jogo->jogador.corpo)) {
+            jogo->jogador.score += jogo->moedas[i].valor;
+            AtualizarRecorde(jogo);
+        }
+    }
+}
+
+static void DesenharMoedasJogo(Jogo *jogo)
+{
+    for (int i = 0; i < jogo->totalMoedas; i++) {
+        DesenharMoeda(jogo->moedas[i]);
+    }
+}
+
+static void AtualizarFase(Jogo *jogo)
+{
+    if (jogo->jogador.linha != 0) {
+        jogo->tocouTopoNestaFase = false;
+        return;
+    }
+
+    if (jogo->tocouTopoNestaFase) {
+        return;
+    }
+
+    jogo->tocouTopoNestaFase = true;
+
+    if (jogo->faseAtual < TOTAL_FASES_ATUAIS) {
+        jogo->faseAtual++;
+        ConfigurarFase(jogo);
+    }
+}
+
+void IniciarJogo(Jogo *jogo)
+{
+    IniciarJogador(&jogo->jogador);
+    jogo->faseAtual = 1;
+    jogo->tocouTopoNestaFase = false;
+    ConfigurarFase(jogo);
     jogo->gameOver = false;
     jogo->jogoIniciado = false;
     jogo->venceu = false;
@@ -31,10 +119,8 @@ void AtualizarJogo(Jogo *jogo)
     }
 
     AtualizarJogador(&jogo->jogador);
-    if (jogo->jogador.score > jogo->recorde) {
-        jogo->recorde = jogo->jogador.score;
-        SalvarRecorde(jogo->recorde);
-    }
+    AtualizarRecorde(jogo);
+    AtualizarMoedas(jogo);
 
     AtualizarListaObstaculos(jogo->obstaculos);
 
@@ -42,10 +128,7 @@ void AtualizarJogo(Jogo *jogo)
         VoltarJogadorCheckpoint(&jogo->jogador);
     }
 
-    if (jogo->jogador.linha == 0) {
-        jogo->venceu = true;
-        jogo->gameOver = true;
-    }
+    AtualizarFase(jogo);
 }
 
 void DesenharJogo(Jogo *jogo)
@@ -56,6 +139,7 @@ void DesenharJogo(Jogo *jogo)
     }
 
     DesenharMapa();
+    DesenharMoedasJogo(jogo);
     DesenharListaObstaculos(jogo->obstaculos);
     DesenharJogador(jogo->jogador);
 
@@ -78,10 +162,11 @@ void DesenharInterface(Jogo *jogo)
     DrawRectangle(0, 0, LARGURA_TELA, 82, Fade(RAYWHITE, 0.75f));
     DrawText(TextFormat("Score: %d", jogo->jogador.score), X_INTERFACE, Y_SCORE, 20, BLACK);
     DrawText(TextFormat("Recorde: %d", jogo->recorde), 140, Y_SCORE, 20, BLACK);
+    DrawText(TextFormat("Fase: %d", jogo->faseAtual), 330, Y_SCORE, 20, BLACK);
     DrawText("WASD para mover", X_INTERFACE, Y_DICA_MOVIMENTO, TAM_TEXTO_INTERFACE, BLACK);
     DrawText("R para reiniciar", X_INTERFACE, Y_DICA_REINICIO, TAM_TEXTO_INTERFACE, BLACK);
     DrawText("P para pausar", 220, Y_DICA_REINICIO, TAM_TEXTO_INTERFACE, BLACK);
-    DrawText("Evite carros e buracos", 565, 10, TAM_TEXTO_INTERFACE, BLACK);
+    DrawText("Evite veiculos e colete moedas", 480, 10, TAM_TEXTO_INTERFACE, BLACK);
 
     if (jogo->pausado) {
         DrawText("PAUSADO", 335, 285, 34, YELLOW);
