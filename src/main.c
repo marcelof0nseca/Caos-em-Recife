e1e395ad1f28d511ee2fc33a7fc03698faf9f620
marcelof0nseca <maf@cesar.school>
2026-05-04 11:20:36 -0300

    feat: adiciona tela cheia sem borda

--- src/main.c
+++ src/main.c
@@ -3,13 +3,60 @@
 #include "jogo.h"
 #include "mapa.h"
 
+static bool telaCheiaSemBorda = false;
+static Vector2 posicaoJanela = {0};
+static int larguraJanela = LARGURA_TELA;
+static int alturaJanela = ALTURA_TELA;
+
+static Rectangle CalcularDestinoTela(void)
+{
+    float escalaX = (float)GetScreenWidth() / LARGURA_TELA;
+    float escalaY = (float)GetScreenHeight() / ALTURA_TELA;
+    float escala = escalaX < escalaY ? escalaX : escalaY;
+    float largura = LARGURA_TELA * escala;
+    float altura = ALTURA_TELA * escala;
+
+    return (Rectangle){
+        (GetScreenWidth() - largura) * 0.5f,
+        (GetScreenHeight() - altura) * 0.5f,
+        largura,
+        altura
+    };
+}
+
+static void AlternarTelaCheia(void)
+{
+    int monitor = GetCurrentMonitor();
+    Vector2 posicaoMonitor = GetMonitorPosition(monitor);
+
+    if (telaCheiaSemBorda) {
+        ClearWindowState(FLAG_WINDOW_UNDECORATED);
+        SetWindowSize(larguraJanela, alturaJanela);
+        SetWindowPosition((int)posicaoJanela.x, (int)posicaoJanela.y);
+        telaCheiaSemBorda = false;
+        return;
+    }
+
+    posicaoJanela = GetWindowPosition();
+    larguraJanela = GetScreenWidth();
+    alturaJanela = GetScreenHeight();
+    SetWindowState(FLAG_WINDOW_UNDECORATED);
+    SetWindowPosition((int)posicaoMonitor.x, (int)posicaoMonitor.y);
+    SetWindowSize(GetMonitorWidth(monitor), GetMonitorHeight(monitor));
+    telaCheiaSemBorda = true;
+}
+
 int main(void)
 {
     Jogo jogo = {0};
+    RenderTexture2D telaVirtual;
 
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(LARGURA_TELA, ALTURA_TELA, "Caos em Recife");
     SetTargetFPS(60);
     ChangeDirectory(GetApplicationDirectory());
+    telaVirtual = LoadRenderTexture(LARGURA_TELA, ALTURA_TELA);
+    SetTextureFilter(telaVirtual.texture, TEXTURE_FILTER_POINT);
 
     InicializarMapa();
     InicializarSpritesJogador();
@@ -18,6 +65,10 @@ int main(void)
     IniciarJogo(&jogo);
 
     while (!WindowShouldClose()) {
+        if (IsKeyPressed(KEY_F11)) {
+            AlternarTelaCheia();
+        }
+
         if (!jogo.jogoIniciado) {
             if (IsKeyPressed(KEY_ENTER)) {
                 jogo.jogoIniciado = true;
@@ -29,12 +80,25 @@ int main(void)
             AtualizarJogo(&jogo);
         }
 
-        BeginDrawing();
+        BeginTextureMode(telaVirtual);
         ClearBackground(RAYWHITE);
         DesenharJogo(&jogo);
+        EndTextureMode();
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawTexturePro(
+            telaVirtual.texture,
+            (Rectangle){0, 0, LARGURA_TELA, -ALTURA_TELA},
+            CalcularDestinoTela(),
+            (Vector2){0, 0},
+            0.0f,
+            WHITE
+        );
         EndDrawing();
     }
 
+    UnloadRenderTexture(telaVirtual);
     FinalizarMapa();
     FinalizarTexturasObstaculo();
     FinalizarTexturaMoeda();
